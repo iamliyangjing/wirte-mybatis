369fa6e15c647016999e10f811777b4c6d56b204
18381826414 <liyl778@163.com>
2023-06-12 23:00:18 +0800

    init

---
+++ src/test/java/com/lyj/writemybatis/WriteMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyj.writemybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
